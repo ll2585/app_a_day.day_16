dc82d9b0c5cab74561fa5f35a4675c917b4f8880
Luke Li <luke.li@nera.com>
2015-04-07 16:28:51 -0400

    Initial commit

---
+++ app/src/main/java/com/lukeli/appaday/day16/MainActivity.java
@@ -0,0 +1,30 @@
+package com.lukeli.appaday.day16;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import com.lukeli.appaday.day16.stab.SlidingTabLayout;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Layout manager that allows the user to flip through the pages
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // getSupportFragmentManager allows use to interact with the fragments
+        // MyFragmentPagerAdapter will return a fragment based on an index that is passed
+        viewPager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+
+        // Initialize the Sliding Tab Layout
+        SlidingTabLayout slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+
+        // Connect the viewPager with the sliding tab layout
+        slidingTabLayout.setViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/lukeli/appaday/day16/TabFragment1.java
@@ -0,0 +1,21 @@
+package com.lukeli.appaday.day16;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+// Same for TabFragment2 and TabFragment3 except change this
+// R.layout.tab_fragment_1 below
+
+public class TabFragment1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View view = inflater.inflate(R.layout.tab_fragment_1, container, false);
+        return view;
+    }
+
+}
